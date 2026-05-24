122705c5423319e5ba37413531f2990e02893844
Maksim <max.berd.1900@gmail.com>
2026-05-24 22:24:13 +0300

    fix

--- src/main.py
+++ src/main.py
@@ -1,4 +1,3 @@
-import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from src.presentation.api import router as api_router
@@ -16,4 +15,5 @@ app.add_middleware(
 app.include_router(api_router)
 
 if __name__ == "__main__":
+    import uvicorn
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
